84643f38e39277ede2ecb1cec8aacf134b481054
Yuhwan <point325@gmail.com>
2025-11-08 21:00:08 +0900

    Divider 컴포넌트 추가

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "./sections/HeroSection";
 import Navbar from "./components/Navbar";
 import IntroSection from "./sections/IntroSection";
+import Divider from "../components/Divider";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main>
         <HeroSection />
         <IntroSection />
+        <Divider />
       </main>
     </div>
   );
---
+++ app/components/Divider.tsx
@@ -0,0 +1,10 @@
+import DashedDots from "../icons/DashedDots";
+
+export default function Divider() {
+  return (
+    <div className="flex flex-row items-center justify-center gap-[8]">
+      <DashedDots />
+      <DashedDots />
+    </div>
+  );
+}
