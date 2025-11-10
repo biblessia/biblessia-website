1455de8d8491b4cbac5c8274ce108cc39d5d9b55
Yuhwan <point325@gmail.com>
2025-11-10 16:17:08 +0900

    리뷰 아바타 수정

--- app/(home)/sections/TestimonialSection.tsx
+++ app/(home)/sections/TestimonialSection.tsx
@@ -46,48 +46,55 @@ function TestimonialList() {
       name: "윤수현",
       church: "엘사랑교회",
       review:
-        "수많은 어플을 쓰면서 한번도 리뷰를 써본적 없는데.. 우연히 남자친구가 같이 해보자고 해서 처음 접했는데 너무 좋아요. 정말 심플하지만 디테일하고 특히 마지막에 기도문이 참 위로가 되고 부담없이",
+        "주일날 교회에서 예배드리기만 했던 선데이 크리스천인데, 사랑의 공동체를 만나서 매일 말씀을 통한 은혜와 감사를 나누고 있어요. 조금씩 큐티도 해보고 기도도 해보며 하나님을 깊이 알아가고 있어요. 혼자서 큐티를 매일 하는 건 쉬운 일이 아닌데, 사랑의 공동체와 함께 하니까 가능하더라고요. 예전에는 일주일에 한번 예배를 통해 하나님을 만났다면, 이제는 매일 공동체와 함께 하나님을 만납니다. 영적으로 함께하는 공동체가 있음에 감사합니다~",
+    },
+    {
+      avatar: "/images/avatar_3.png",
+      name: "7D맹고",
+      church: "앱 스토어 리뷰",
+      review:
+        "수많은 어플을 쓰면서 한번도 리뷰를 써본적 없는데.. 우연히 남자친구가 같이 해보자고 해서 처음 접했는데 너무좋아요 정말 심플하지만 디테일하고 특히 마지막에 기도문이 참 위로가 되고 부담없이 핸드폰을 들고도 묵상하며 기도하고 감사할 수 있다는것이 정말 놀랍고 너무 좋습니다...!\n\n감사 일기도 세개를 쓰고나면 계속 하나씩 더 쓸 수 있게 되어있는것도 부담없이 생각나는대로 적을 수 있어서 좋았어요. 제가 다시 말씀과 기도의 자리에 설 수 있도록 이끌어주셔서 감사합니다.",
     },
     {
       avatar: "/images/avatar_2.png",
       name: "최형욱",
       church: "컴앤씨교회",
       review:
-        "When we began building this colony, I was skeptical about how we could make sure the right bugs would find us to join in. Namedly's integrated solution exceeded all my expectations.",
+        "스무살에 처음 큐티 배우고 30년 간 전문적인 세미나와 훈련만 열번 넘게 참여했지만 왜 매일 할 수 없는 이유만 자꾸 늘어 가는지… 성경 읽기도, 기도 시간도,감사 일기도 마찬가지이지만 작심삼일… 결국 안되는 건가 포기하고 있을 때 찾아 온 하늘의 비밀 병기 비블레시아가 휴대폰에 장착되었다.\n\n할렐루야! 이제 늦게 일어나도, 출장이나 여행으로 성경책이나 큐티 노트, 해설서가 없어도 그냥 다 되네! 소그룹이나 공동체 설정하면 한번만 기록해도 원하는 곳에 나눌 수 있고, 서로 격려와 중보기도로 함께 자라갈 수 있는 비블레시아로 지금 몇 달 째 하루도 빠짐없이 말씀과 함께 살게 되었다. ",
     },
     {
-      avatar: "/images/avatar_3.png",
-      name: "손은지",
-      church: "서울홍성교회",
-      review:
-        "It’s a delicate time when butterflies emerge. They’re searching for their place among the leaves. And that’s where Namedly popped in to help.",
-    },
-    {
-      avatar: "/images/avatar_5.png",
-      name: "여성진",
-      church: "강남교회",
+      avatar: "/images/avatar_7.png",
+      name: "곽은주",
+      church: "컴앤씨교회",
       review:
         "I’ve been transformed completely. I wouldn’t use any other service.",
     },
     {
-      avatar: "/images/avatar_6.png",
-      name: "이승가",
-      church: "주님의교회",
+      avatar: "/images/avatar_4.png",
+      name: "하늘해 그리고",
+      church: "앱스토어 리뷰",
       review:
-        "Namedly's tools for managing our identity through many stages of development—larval, pupal, and far beyond—were top notch. I’ve been through metamorphosis many times, and I was impressed.",
+        "그저 스쳐 지날 수 있는 묵상들과 내 삶의 이야기들을 정리하는 시간을 갖고 공동체에서 서로 나누며 서로를 이해하고 함께 중보할 수 있음에 너무 감사하고 귀한 어플입니다 👏👏👏",
     },
     {
-      avatar: "/images/avatar_4.png",
-      name: "조희정",
-      church: "온누리교회",
+      avatar: "/images/avatar_6.png",
+      name: "라니89",
+      church: "앱스토어 리뷰",
       review:
-        "In the garden of life, some things are just very sweet. This is one of them. Namedly made it so easy to find the flowers we needed in our busy summer season.",
+        "큐티책에서 작성하게 되면 월이 끝나면 책을 보관하는데 어려움이 있어 버리게 될 때가 많은데 나의 큐티묵상을 기록하여 남길 수 있고 필요할때 언제든 찾아볼 수 있어 개인 신앙생활에 도움이 많이 된다!\n\n그리고 공동체로 묶여있는 사람들의 나눔을 보면서 내가 생각지 못한 깨달음을 얻게되고, 댓글과 이모티콘 등으로 나눔을 함께 응원할 수 있어 좋다!",
+    },
+    {
+      avatar: "/images/avatar_5.png",
+      name: "물바우",
+      church: "앱스토어 리뷰",
+      review:
+        "출근길 지하철에서도, 잠시 쉬는 시간에도 마음만 먹으면 쉽게 큐티를 할수 있습니다. 내가 속한 공동체, 가족, 친구와 삶을 나눌수 있습니다. 큐티가 익숙하지 않아도 쉽게 할수 있는 점이 너무 좋습니다.",
     },
   ];
   const chunks = [
     testimonials.slice(0, 2),
     testimonials.slice(2, 4),
-    testimonials.slice(4, 6),
+    testimonials.slice(4, 7),
   ];
 
   return (
@@ -118,7 +125,7 @@ function TestimonialItem({ avatar, name, church, review }: Testimonial) {
           </div>
         </div>
       </div>
-      <p className="text-lg break-keep">{review}</p>
+      <p className="text-lg break-keep whitespace-pre-line">{review}</p>
     </div>
   );
 }
